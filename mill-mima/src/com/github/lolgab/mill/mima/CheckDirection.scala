e7dae1121afd33b3bc905352d52cda05ab884c37
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-11-17 18:02:20 +0100

    .

--- mill-mima/src/com/github/lolgab/mill/mima/CheckDirection.scala
+++ mill-mima/src/com/github/lolgab/mill/mima/CheckDirection.scala
@@ -4,7 +4,7 @@ import upickle.default._
 
 sealed trait CheckDirection extends Product with Serializable
 object CheckDirection {
-  case object Backward extends CheckDirection
+  case object Backward  extends CheckDirection
   case object Forward extends CheckDirection
   case object Both extends CheckDirection
 
